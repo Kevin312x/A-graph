a6f724266f3f93d526df6ec76a29ed6b6a45e29e
Kevin Huynh <anmevee@yahoo.com>
2020-12-01 14:04:00 -0800

    added: algorithm for pathfinding

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const grid = document.querySelector('.grid-container');
 
 const cells_list_2d = [];
-let visited_cells = [];
 let wall_cells = new Set;
 let start_cell;
 let end_cell;
 
 class Cell {
+    g_cost = Number.MAX_SAFE_INTEGER; // Distance from start cell
+    h_cost = Number.MAX_SAFE_INTEGER; // Distance from end cell
+    f_cost = Number.MAX_SAFE_INTEGER; // Sum of g_cost and h_cost
+    parent_cell = null; // Previous cell (for tracing)
     constructor(x_pos, y_pos) {
         this.x_pos = x_pos;
         this.y_pos = y_pos;
@@ -81,6 +84,83 @@ function create_grid() {
             }
         });
     });
+}
+
+function astar_pathfinding() {
+    start_cell.g_cost = 0;
+    start_cell.h_cost = 0;
+    start_cell.f_cost = 0;
+
+    let open = [];
+    let closed = [];
+    open.push(start_cell)
+
+    while(true) {
+        let f_cost_low = open[0];
+        let f_cost_pos = 0;
+
+        for(let i = 1; i < open.length; ++i) {
+            if(open[i].f_cost < f_cost_low.f_cost) {
+                f_cost_low = open[i];
+                f_cost_pos = i;
+            }
+        }
+
+        open.splice(f_cost_pos, 1);
+        closed.push(f_cost_low);
+
+        if(f_cost_low == end_cell) { break; }
+
+        let neighbors = [];
+        let pn_x_axis = [f_cost_low.x_pos - 1, f_cost_low.x_pos, f_cost_low.x_pos + 1];
+        let pn_y_axis = [f_cost_low.y_pos - 1, f_cost_low.y_pos, f_cost_low.y_pos + 1];
+
+        for(let x = 0; x < pn_x_axis.length; ++x) {
+            for(let y = 0; y < pn_y_axis.length; ++y) {
+                if(pn_x_axis[x] < 0 || pn_x_axis[x] > 19 || 
+                    pn_y_axis[y] < 0 || pn_y_axis[y] > 19) { continue; }
+
+                if(pn_x_axis[x] == f_cost_low.x_pos && pn_y_axis[y] == f_cost_low.y_pos) { continue; }
+                neighbors.push(cells_list_2d[pn_x_axis[x]][pn_y_axis[y]]);
+            }
+        }
+
+        for(let i = 0; i < neighbors.length; ++i) {
+            if(wall_cells.has(neighbors[i]) || closed.includes(neighbors[i])) { continue; }
+            document.getElementById(`${neighbors[i].x_pos} ${neighbors[i].y_pos}`).classList.add('visited');
+
+            let g_cost = f_cost_low.g_cost + calc_dist(f_cost_low, neighbors[i]);
+            let h_cost = calc_dist(end_cell, neighbors[i]);
+            let f_cost = g_cost + h_cost;
+
+            if(f_cost < neighbors[i].f_cost) {
+                neighbors[i].g_cost = g_cost;
+                neighbors[i].h_cost = h_cost;
+                neighbors[i].f_cost = f_cost;
+                neighbors[i].parent_cell = f_cost_low;
+            }
+
+            if(!open.includes(neighbors[i])) { open.push(neighbors[i]) }
+        }
+    }
+
+    if(end_cell.parent_cell != null) {
+        let traverse_cell = end_cell.parent_cell;
+        do {
+            let path_cell = document.getElementById(`${traverse_cell.x_pos} ${traverse_cell.y_pos}`);
+            path_cell.style.backgroundColor = 'cyan';
+            traverse_cell = traverse_cell.parent_cell;
+        } while(traverse_cell != start_cell);
+    }
+}
+
+// Calculates distances
+// Also used to 'approximate' h_cost
+function calc_dist(first_cell, second_cell) {
+    // Distance formula = sqrt((x2 - x1)^2 + (y2-y1)^2)
+    let h_cost_estimation = (Math.sqrt(Math.pow((second_cell.x_pos - first_cell.x_pos), 2) + 
+                            Math.pow((second_cell.y_pos - first_cell.y_pos), 2))) * 10;
+    return Math.floor(h_cost_estimation);
 }
 
 function check() {
